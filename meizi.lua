65c647c5562db8469c8d043fc94b1000d98b59f5
Rocks Wang <rockswang@gmail.com>
2020-09-14 17:52:58 +0800

    Update meizi.lua
    不需要@latest

--- meizi.lua
+++ meizi.lua
@@ -13,14 +13,14 @@ function ReceiveGroupMsg(CurrentQQ, data)
     
     local idx = math.random(0, 130)
     idx = string.format('%04d', idx)
-    local url = "https://cdn.jsdelivr.net/gh/ipchi9012/cos_pics@latest/cos_" .. idx .. ".js"
+    local url = "https://cdn.jsdelivr.net/gh/ipchi9012/cos_pics/cos_" .. idx .. ".js"
     log.info("url=%s", url)
     local resp = http.request("GET", url).body
     idx = string.find(resp, "(", 1, true)
     resp = string.sub(resp, idx + 1, -2)
 		resp = json.decode(resp)
     local item = resp[math.random(table.getn(resp))]
-    local url = "https://cdn.jsdelivr.net/gh/ipchi9012/cos_pics@latest/" .. item.path
+    local url = "https://cdn.jsdelivr.net/gh/ipchi9012/cos_pics/" .. item.path
     local text = item.category .. " - " .. item.suite .. "\n" .. url
     log.info("url=%s, text=%s", url, text)
     ApiRet = Api.Api_SendMsg(
